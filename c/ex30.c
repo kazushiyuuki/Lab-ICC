de5cd7cae1ec2eceeee36a8d44c3486f194f62c4
Roberto Kazushi Yuuki Junior <kazushiyuuki@gmail.com>
2020-04-02 23:40:19 -0300

    Arrumando a organização dos exercício em C
    - Add Exercício 30 - Os dois maiores dos N inteiros lidos
    - Rmv Exercícios aleatórios que estavam no repositório

---
+++ c/ex30.c
@@ -0,0 +1,30 @@
+//Esse algoritmo lê N inteiros  e omprime os dois maiores dos lidos em ordem decrescente
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n, maior1, maior2, aux, num=0;
+    setbuf(stdin, NULL);
+    scanf(" %i", &n);
+    aux = n;
+    while(n>=1){
+        setbuf(stdin, NULL);
+        scanf(" %i", &num);
+        if(aux == n){
+            maior1 = num;
+        }
+        else if((aux-1) == n){
+            maior2 = num;
+        }
+        if(num > maior1){
+            maior2 = maior1;
+            maior1 = num;
+        }
+        else if(num > maior2){
+            maior2 = num;
+        }
+        n--;
+    }
+
+    printf("%i\n%i", maior1, maior2);
+    return 0;
+}
