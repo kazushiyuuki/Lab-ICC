d6a4b0a495b8853cdfb90731e0e933a3b9de0d70
Roberto Kazushi Yuuki Junior <kazushiyuuki@gmail.com>
2020-05-22 19:18:13 -0300

    Update trab01.c

--- trabalho3/trab01.c
+++ trabalho3/trab01.c
@@ -1,1 +1,153 @@
+
 //Jogo da velha, 3x3
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void iniciarTabuleiro(char tabuleiro[3][3])
+{
+    int i, j;
+    for(i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            tabuleiro[i][j] = ' ';
+        }
+    }
+}
+
+int verificar(char tabuleiro[3][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        if(tabuleiro [i][0] != ' ' && tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1]== tabuleiro[i][2])
+        {
+            if(tabuleiro[i][0] == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+             
+        }
+        else if(tabuleiro [0][i] != ' ' && tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i])
+        {
+            if(tabuleiro[0][i] == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if(tabuleiro[0][0] != ' ' && tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2])
+        {
+            if(tabuleiro[0][0] == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if(tabuleiro[2][0] != ' ' && tabuleiro[2][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[0][2])
+        {
+            if(tabuleiro[2][0] == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    return -1;
+}
+
+void inserirJogador(char tabuleiro[3][3])
+{
+    printf("Digite a posicao desejada: ");
+    int x, y;
+    scanf("%d %d", &x, &y);
+    tabuleiro[x][y] = 'X';
+}
+
+void inserirComputador(char tabuleiro[3][3])
+{
+    srand(time(NULL));
+    int aux = 0;
+    while(aux == 0)
+    {
+        int x = rand() % 3;
+        int y = rand() % 3;
+        if(tabuleiro[x][y] == ' ')
+        {
+            tabuleiro[x][y] = 'O';
+            aux = 1;
+        }
+    }
+}
+
+void imprimirTabuleiro(char tabuleiro[3][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        printf("%c | %c | %c\n", tabuleiro[i][0], tabuleiro[i][1], tabuleiro[i][2]);
+    }
+}
+
+
+int main()
+{
+    char tabuleiro[3][3];
+    char controle = 'S';
+    inserirComputador(tabuleiro);
+    while(controle == 'S')
+    {
+        iniciarTabuleiro(tabuleiro);
+        imprimirTabuleiro(tabuleiro);
+        int jogadas = 0, resultado = -1;
+        while(jogadas < 9 && resultado == -1)
+        {
+            inserirJogador(tabuleiro);
+            resultado = verificar(tabuleiro);
+            if(resultado == 1)
+            {
+                printf("X\n");
+            }
+            else if(resultado == 0)
+            {
+                printf("O\n");
+            }else
+            {
+                inserirComputador(tabuleiro);
+                resultado = verificar(tabuleiro);
+                if(resultado == 1)
+                {
+                    printf("X\n");
+                }
+                else if(resultado == 0)
+                {
+                    printf("O\n");
+                }
+                else
+                {
+                    imprimirTabuleiro(tabuleiro);
+                }
+            }
+
+        }
+        if(jogadas == 9)
+        {
+            printf("Empatou\n");
+        }
+        printf("Deseja jogar novamente: ");
+        scanf(" %c", &controle);
+    }
+}
