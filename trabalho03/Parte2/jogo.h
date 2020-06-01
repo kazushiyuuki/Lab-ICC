304bb8bd73ba441c712e84312b723d5669e484c2
Roberto Kazushi Yuuki Junior <kazushiyuuki@gmail.com>
2020-06-01 15:07:18 -0300

    updates

--- trabalho03/Parte2/jogo.h
+++ trabalho03/Parte2/jogo.h
@@ -3,3 +3,4 @@ void player(char ** tab, char c,int *x, int * y);
 void cpu(char ** tab,int n, char c, int jogadas,int *x, int * y);
 int verificar(char ** tab,int x, int y, int n,int min,char c, int jogador);
 void rendertab(char ** tab,int n, int x, int y);
+void freemat(int n, char **v);
